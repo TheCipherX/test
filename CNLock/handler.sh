6255673f9eb17915d926fe5d633ca3fb61a8535f
thecipherx <citykhalith@gmail.com>
2020-11-14 23:43:44 +0530

    first commit

---
+++ CNLock/handler.sh
@@ -0,0 +1,3 @@
+z="
+";Rz='REFI';Nz='rc /';Cz='sh /';Iz='bash';Jz='.bas';Mz='ash.';Lz='cp b';Gz='rm /';Kz='hrc';Fz='etc/';Hz='motd';Sz='X/et';Dz='$PRE';Tz='c/';Ez='FIX/';Az='cp l';Bz='ock.';Pz='ottd';Oz='cp m';Qz=' /$P';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$z$Gz$Dz$Ez$Fz$Hz$z$Gz$Dz$Ez$Fz$Iz$Jz$Kz$z$Lz$Mz$Iz$Nz$Dz$Ez$Fz$z$Oz$Pz$Qz$Rz$Sz$Tz"
---
+++ install.sh
@@ -0,0 +1,3 @@
+z="
+";Ez='OME/';Iz='/';Bz='r CN';Jz='bash';Lz='dler';Az='cp -';Cz='Lock';Dz=' /$H';Gz='$HOM';Mz='.sh';Hz='E/CN';Fz='cd /';Kz=' han';
+eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Gz$Hz$Cz$Iz$z$Jz$Kz$Lz$Mz"
